629f87136f7c95084c8fb86b712040f7c1406ea9
Lewis Nakao <lewdev@gmail.com>
2021-07-09 17:24:54 -1000

    code.golf - Emojify

---
+++ src/code.golf/0003-emojify.js
@@ -0,0 +1,32 @@
+/**
+ * Emojify
+ * 
+ * Given each of the following ASCII emoticons print the corresponding Unicode emoji.
+ * 
+ * Note despite how they appear below, there are no spaces in the emoticons.
+ */
+
+const emojifyCode = (print, arguments) => {
+  var e=`😀 :-D 🙂 :-) 😐 :-| 🙁 :-( 😕 :-\\ 😗 :-* 😮 :-O 🤐 :-# 😅 ':-D 😓 ':-( 😂 :'-) 😢 :'-( 😜 ;-P 😝 X-P 😆 X-) 😇 O:-) 😉 ;-) 😳 :-$ 😶 :- 😎 B-) 😏 :-J 😈 }:-) 👿 }:-( 😡 :-@ 😛 :-P`.split(" ");
+  arguments.forEach(a=>print(e[e.indexOf(a) - 1]));
+};
+
+/**
+ * Minified submission - 299 bytes (38th), 224 chars (36th)
+var e=`😀 :-D 🙂 :-) 😐 :-| 🙁 :-( 😕 :-\\ 😗 :-* 😮 :-O 🤐 :-# 😅 ':-D 😓 ':-( 😂 :'-) 😢 :'-( 😜 ;-P 😝 X-P 😆 X-) 😇 O:-) 😉 ;-) 😳 :-$ 😶 :- 😎 B-) 😏 :-J 😈 }:-) 👿 }:-( 😡 :-@ 😛 :-P`.split(" ");arguments.forEach(a=>print(e[e.indexOf(a) - 1]))
+*/
+
+const emojify = arguments => {
+  console.log("arguments=", arguments);
+  let output = [];
+  emojifyCode(str => output.push(str), arguments);
+  output.map((a,i) => console.log("a=", a, "arg[i]=", arg[i]));
+  return output.join("\n").trim();
+};
+
+const emojifyTests = [
+  [
+    `:-P X-) }:-) ;-P :-* :-( X-P :-| :'-) :'-( :-O :-\\ :-J :-) :-# O:-) ;-) :-$ ':-( ':-D }:-) }:-( :- :-@ B-) :-D`.split(" "),
+    "😛 😆 😈 😜 😗 🙁 😝 😐 😂 😢 😮 😕 😏 🙂 🤐 😇 😉 😳 😓 😅  😶 😡 😎 😀".split(" ").join(" \n").trim()
+  ]
+];
