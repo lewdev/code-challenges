d57f4f0bf4b5eaea847c6c15d33983da442acc30
Lewis Nakao <lewis.nakao@polarisalpha.com>
2021-11-05 00:48:00 -1000

    LC 43. Multiply Strings (Medium)

--- src/leetcode/0043-multiply.js
+++ src/leetcode/0043-multiply.js
@@ -1,29 +1,60 @@
 const multiply = (num1, num2) => {
+  if (num1 === "0" || num2 === "0") return "0";
+
   const n1Arr = num1.split("").reverse();
   const n2Arr = num2.split("").reverse();
-  console.log(`\n\n\n${num1} * ${num2}`);
-  let n1, n2, product = 0, output = "", carry = 0;
-  for (let i = 0; i < n1Arr.length; i++) {
+  let longer, shorter, n1Longer = n1Arr.length > n2Arr.length;
+  longer = n1Longer ? n1Arr : n2Arr;
+  shorter = !n1Longer ? n1Arr : n2Arr;
+
+  let n1, n2, product = 0, output = "", carry = 0, sum = "0";
+  for (let i = 0; i < shorter.length; i++) {
     product = 0;
     carry = 0;
-    n1 = parseInt(n1Arr[i]);
-    for (let j = 0; j < n2Arr.length; j++) {
-      n2 = parseInt(n2Arr[j]) * Math.pow(10, j);
-      if (!n2 || !(n1 * n2)) continue;
-      product += (n1 * n2) + carry;
-      console.log(n1, " * ", n2, " = ", (n1 * n2), ", carry=", carry, ", output=", output);
-      if (product > 9 && i < n1Arr.length - 1) {
+    output = "";
+    n1 = (shorter[i] ? parseInt(shorter[i]) : 0);
+    for (let j = 0; j < longer.length; j++) {
+      n2 = parseInt(longer[j]);
+      product = (n1 * n2) + carry;
+      if (product > 9 && j < longer.length - 1) {
         carry = Math.floor(product / 10);
         product = product % 10;
       }
       else carry = 0;
+      output = product + output;
     }
-    output = product + output;
+
+    //add the "0"s after
+    let k = i;
+    while (k--) output += "0";
+
+    //add products together
+    sum = add(sum, output);
   }
-  while (output.charAt(0) === "0") {
-    output = output.substr(1);
+  return sum ? sum : "0";
+};
+
+const add = (num1, num2) => {
+  if (num1 === "0") return num2;
+  if (num2 === "0") return num1;
+
+  const n1Arr = num1.split("").reverse();
+  const n2Arr = num2.split("").reverse();
+  let longer, shorter, n1Longer = n1Arr.length > n2Arr.length;
+  longer = n1Longer ? n1Arr : n2Arr;
+  shorter = !n1Longer ? n1Arr : n2Arr;
+
+  let carry = 0, sum, output = "";
+  for (let i = 0; i < longer.length; i++) {
+    sum = (shorter[i] ? parseInt(shorter[i]) : 0) + parseInt(longer[i]) + carry;
+    if (sum > 9 && i < longer.length - 1) {
+      carry = 1;
+      sum -= 10;
+    }
+    else carry = 0;
+    output = sum + output;
   }
-  return output ? output : "0";
+  return output;
 };
 
 const multiplyTests = [
@@ -34,6 +65,7 @@ const multiplyTests = [
   [["1200", "62176"], "74611200"],
   [["725071900", "6478"], "4697015768200"],
   [["18582506933032752", "366213329703"], "6805161738175006549873432656"],
+  [["96423702883453279", "72156405165936898"], "6957587772858372748255887645188542"]
 ];
 /*
 6805161738175006549873432656
